9820272c9a317e8f963c4cf58f4bf32bb0a5654d
Mradima2000 <59370519+Mradima2000@users.noreply.github.com>
2020-10-17 23:00:32 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       title: 'Startup Name Generator',
       theme: ThemeData(          // Add the 3 lines from here... 
         primaryColor: Colors.white,
-      ), 
+      ),
       home: RandomWords(),
     );
   }
@@ -116,7 +116,7 @@ class RandomWordsState extends State<RandomWords> {
   // #docregion RWS-var
 }
 // #enddocregion RWS-var
-
+// statefulwidget section
 class RandomWords extends StatefulWidget {
   @override
   RandomWordsState createState() => RandomWordsState();
